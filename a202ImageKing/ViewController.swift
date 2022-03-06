a62f0ce21820a82a99680f2f2d44c4b35279b70d
Danny Shen <shenfive@gmail.com>
2022-03-06 13:39:30 +0800

    sdWimafe

--- a202ImageKing/ViewController.swift
+++ a202ImageKing/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Kingfisher
+import SDWebImage
 
 class ViewController: UIViewController {
 
@@ -19,19 +19,17 @@ class ViewController: UIViewController {
         theImage.layer.cornerRadius = theImage.bounds.width / 2
         theImage.clipsToBounds = true
         
-
-
-        
-        
-        
         let path = "https://attach.setn.com/newsimages/2019/12/28/2328596-PH.jpg"
         let url = URL(string: path)!
         
-        
         print("start:\(Date().timeIntervalSince1970))")
         
+        theImage.sd_setImage(with: url) { image, error, type, url in
+            print("endImage:\(Date().timeIntervalSince1970))")
+        }
+
         
-        self.theImage.kf.setImage(with: url)
+        
 
         print("endUI:\(Date().timeIntervalSince1970))")
     }
