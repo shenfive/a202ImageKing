dfea2051e05686d792764eeb7aff31e48868d0d0
Danny Shen <shenfive@gmail.com>
2022-03-06 13:28:04 +0800

    init

--- a202ImageKing/ViewController.swift
+++ a202ImageKing/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        theImage.layer.borderColor = UIColor.gray.cgColor
+        theImage.layer.borderWidth = 1
+        theImage.layer.cornerRadius = theImage.bounds.width / 2
+        theImage.clipsToBounds = true
+        
+
+
+        
+        
+        
+        let path = "https://attach.setn.com/newsimages/2019/12/28/2328596-PH.jpg"
+        let url = URL(string: path)!
+        
+        
+        print("start:\(Date().timeIntervalSince1970))")
+        
+        
+        self.theImage.kf.setImage(with: url)
+
+        print("endUI:\(Date().timeIntervalSince1970))")
     }
 
 
